d88d3012149464e614320e19f2c3c050c9974887
NoviScl <sichenglei1125@gmail.com>
2023-10-21 23:45:31 -0700

    gpt4 examples

--- screenshot.py
+++ screenshot.py
@@ -1,18 +1,23 @@
 from playwright.sync_api import sync_playwright
+import os
 
 def take_screenshot(url, output_file="screenshot.png"):
+    # Convert local path to file:// URL if it's a file
+    if os.path.exists(url):
+        url = "file://" + os.path.abspath(url)
+
     with sync_playwright() as p:
         # Choose a browser, e.g., Chromium, Firefox, or WebKit
         browser = p.chromium.launch()
         page = browser.new_page()
-        
+
         # Navigate to the URL
         page.goto(url)
-        
+
         # Take the screenshot
         page.screenshot(path=output_file, full_page=True)
-        
+
         browser.close()
 
-# Example usage
-take_screenshot("https://cs.stanford.edu/~diyiy/research.html", "diyi_screenshot.png")
+for name in ["aryaman", "danqi", "diyi", "tatsu", "yanzhe"]:
+    take_screenshot("/Users/clsi/Desktop/Pix2Code/trial_dataset/" + "{}.html".format(name), "trial_dataset/" + "{}.png".format(name))
