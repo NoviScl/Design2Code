579f368ab6c34a1176ae7926970f9b89f4611e9b
NoviScl <sichenglei1125@gmail.com>
2023-10-27 11:42:43 -0700

    c4 scraping

--- crawl_w_css.py
+++ crawl_w_css.py
@@ -1,6 +1,10 @@
 from playwright.sync_api import sync_playwright
 import requests
 import re
+import json
+from tqdm import tqdm
+import random 
+random.seed(2023)
 
 placeholder_image = "rick.jpg"
 
@@ -9,13 +13,16 @@ def replace_img_src(input_string):
     replacement = r'\1' + placeholder_image + r'\2'
     return re.sub(pattern, replacement, input_string)
 
-def fetch_and_embed_css(url):
+def fetch_and_embed_css(url, navigation_timeout=2000, request_timeout=10):
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
 
-        # Navigate to the URL
-        page.goto(url)
+        # Navigate to the URL with timeout
+        try:
+            page.goto(url, timeout=navigation_timeout)
+        except:
+            return None
 
         # Extract stylesheets' hrefs using JavaScript
         stylesheets_hrefs = page.eval_on_selector_all("link[rel='stylesheet']", 'nodes => nodes.map(n => n.href)')
@@ -24,10 +31,13 @@ def fetch_and_embed_css(url):
         # Fetch and embed each external CSS
         content = page.content()
         for href in stylesheets_hrefs:
-            css_content = requests.get(href).text
-            # print (href)
-            # print (css_content)
-            inline_css += '\n' + css_content + '\n\n'
+            try:
+                response = requests.get(href, timeout=request_timeout)
+                response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
+                css_content = response.text
+                inline_css += '\n' + css_content + '\n\n'
+            except:
+                return None
 
         if "<style>" in content:
             content = content.replace("<style>", "<style>\n" + inline_css)
@@ -40,7 +50,21 @@ def fetch_and_embed_css(url):
 
         return content
 
-# Example usage
-html_content = fetch_and_embed_css("https://aryaman.io/")
-with open("trial_dataset/aryaman.html", "w", encoding="utf-8") as f:
-    f.write(html_content)
+c4 = "../c4-validation.00000-of-00008.json"
+urls = []
+with open(c4, 'r') as f:
+    for line in f:
+        d = json.loads(line)
+        urls.append(d["url"])
+
+urls = list(set(urls))[:10]
+
+counter = 1
+for i, url in tqdm(enumerate(urls)):
+    html_content = fetch_and_embed_css(url)
+    if html_content:
+        with open("c4-val-00000/{}.html".format(counter), "w", encoding="utf-8") as f:
+            f.write(html_content)
+        counter += 1
+    
+
