bcffc6094adf859ae39e90d26bba50084a01927d
Yanzhe Zhang <51267590+StevenyzZhang@users.noreply.github.com>
2023-11-21 09:14:04 -0800

    Update visual eval on pilot testset

---
+++ syn_dataset/screenshot_batch.py
@@ -0,0 +1,52 @@
+import os
+from playwright.sync_api import sync_playwright
+import os
+
+def take_screenshot(url, output_file="screenshot.png"):
+    # Convert local path to file:// URL if it's a file
+    if os.path.exists(url):
+        url = "file://" + os.path.abspath(url)
+
+    if os.path.exists(output_file):
+        print(f"{output_file} exists!")
+        return
+
+    with sync_playwright() as p:
+        # Choose a browser, e.g., Chromium, Firefox, or WebKit
+        browser = p.chromium.launch()
+        page = browser.new_page()
+
+        # Navigate to the URL
+        page.goto(url)
+
+        # Take the screenshot
+        page.screenshot(path=output_file, full_page=True)
+
+        browser.close()
+
+    print(f"Saved to {output_file}")
+
+def process_files_with_prefix(folder, prefix, processing_function):
+    """
+    Process files in a folder that start with a given prefix using a specified function.
+
+    Parameters:
+    folder (str): The folder to search in.
+    prefix (str): The prefix to filter files by.
+    processing_function (function): The function to apply to each file.
+    """
+    # Check if the folder exists
+    if not os.path.exists(folder):
+        print(f"Folder '{folder}' does not exist.")
+        return
+
+    # List all files in the folder
+    for filename in os.listdir(folder):
+        # Check if the filename starts with the prefix
+        if filename.startswith(prefix):
+            # Construct the full path
+            file_path = os.path.join(folder, filename)
+            # Process the file using the provided function
+            processing_function(file_path, file_path.replace(".html", ".png"))
+
+process_files_with_prefix("/Users/zhangyanzhe/Documents/GitHub/Pix2Code/pilot_testset", "gpt4v", take_screenshot)
