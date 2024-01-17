61f231af640a7f6c14603205b06197c35ba4bd67
NoviScl <sichenglei1125@gmail.com>
2024-01-16 21:20:26 -0800

    more predictions

--- Pix2Code/metrics/visual_score.py
+++ Pix2Code/metrics/visual_score.py
@@ -14,7 +14,7 @@ from PIL import Image, ImageDraw
 import torch
 import clip
 
-from ocr_free_utils import get_blocks_ocr_free
+from Pix2Code.metrics.ocr_free_utils import get_blocks_ocr_free
 
 # pytesseract.pytesseract.tesseract_cmd = '/sailhome/clsi/bin/tesseract'
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -595,9 +595,6 @@ if __name__ == "__main__":
     all_scores = 0
     counter = 0
 
-    print("Visual Eval V1 applied!")
-    print("Last update: 01/08/24 -- Yanzhe")
-    
     for filename in tqdm([item for item in os.listdir(predictions_dir) if item.endswith(".html")]):
         if filename.endswith(".html"):
             # matched, loc_score, size_score, final_score = visual_eval(os.path.join(predictions_dir, filename.replace(".html", ".png")), os.path.join(reference_dir, filename.replace(".html", ".png")))
