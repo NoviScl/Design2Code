622274fd83808fb48b3f4102264ba9213043b6ed
NoviScl <sichenglei1125@gmail.com>
2024-01-29 14:12:59 -0800

    layout_marker prompting

--- Pix2Code/prompting/gpt4v.py
+++ Pix2Code/prompting/gpt4v.py
@@ -2,7 +2,7 @@ import requests
 import os
 from tqdm import tqdm
 from Pix2Code.data_utils.screenshot import take_screenshot
-from gpt4v_utils import cleanup_response, encode_image, gpt_cost, extract_text_from_html
+from gpt4v_utils import cleanup_response, encode_image, gpt_cost, extract_text_from_html, index_text_from_html
 import json
 from openai import OpenAI, AzureOpenAI
 import argparse
@@ -32,7 +32,8 @@ def gpt4v_call(openai_client, base64_image, prompt):
 			}
 		],
 		max_tokens=4000,
-		temperature=0.0
+		temperature=0.0,
+		seed=2024
 	)
 
 	prompt_tokens, completion_tokens, cost = gpt_cost("gpt-4-vision-preview", response.usage)
@@ -79,7 +80,8 @@ def gpt4v_revision_call(openai_client, base64_image_ref, base64_image_pred, prom
 			}
 		],
 		max_tokens=4000,
-		temperature=0.0
+		temperature=0.0,
+		seed=2024
 	)
 	
 	prompt_tokens, completion_tokens, cost = gpt_cost("gpt-4-vision-preview", response.usage)
@@ -96,7 +98,8 @@ def gpt4_call(openai_client, prompt, model="gpt-4-1106", temperature=0., max_tok
         messages=prompt_messages,
         temperature=temperature,
         max_tokens=max_tokens,
-        response_format=response_format
+        response_format=response_format,
+		seed=2024
     )
 	cost = gpt_cost(model, completion.usage)
 	response = completion.choices[0].message.content.strip()
@@ -133,9 +136,6 @@ def text_augmented_prompting(openai_client, image_file):
 	'''
 	{original input image + extracted text + prompt} -> {output html}
 	'''
-
-	## encode the image 
-	base64_image = encode_image(image_file)
 
 	## extract all texts from the webpage 
 	with open(image_file.replace(".png", ".html"), "r") as f:
@@ -196,13 +196,87 @@ def visual_revision_prompting(openai_client, input_image_file, original_output_i
 
 	return html, prompt_tokens, completion_tokens, cost
 
+def layout_marker_prompting(openai_client, image_file, auto_insertion=False):
+	'''
+	{marker image + extracted text + prompt} -> {output html}
+	'''
+
+	orig_input_image = encode_image(image_file)
+
+	## extract all texts from the webpage 
+	with open(image_file.replace(".png", ".html"), "r") as f:
+		html_content = f.read()
+	marker_html_content, text_dict = index_text_from_html(html_content)
+
+	#save the marker html content
+	with open(image_file.replace(".png", "_marker.html"), "w") as f:
+		f.write(marker_html_content)
+	take_screenshot(image_file.replace(".png", "_marker.html"), image_file.replace(".png", "_marker.png"))
+	oracle_marker_image = encode_image(image_file.replace(".png", "_marker.png"))
+
+	texts = ""
+	for index, text in text_dict.items():
+		texts += f"[{index}] {text}\n"
+
+	## the layout generation prompt
+	text_augmented_prompt = ""
+	text_augmented_prompt += "You are an expert web developer who specializes in HTML and CSS.\n"
+	text_augmented_prompt += "A user will provide you with a screenshot of a webpage where all text elements should be index markers.\n"
+	text_augmented_prompt += "The original text elements are:\n" + texts + "\n"
+	text_augmented_prompt += "You should generate the correct layout structure for the webpage, and put the markers in the correct places. Markers should be wrapped in square backets like \"[1]\".\n"
+	text_augmented_prompt += "You need to return a single html file that uses HTML and CSS to reproduce the given website.\n"
+	text_augmented_prompt += "Include all CSS code in the HTML file itself.\n"
+	text_augmented_prompt += "If it involves any images, use \"rick.jpg\" as the placeholder.\n"
+	text_augmented_prompt += "Some images on the webpage are replaced with a blue rectangle as the placeholder, use \"rick.jpg\" for those as well.\n"
+	text_augmented_prompt += "Do not hallucinate any dependencies to external files. You do not need to include JavaScript scrips for dynamic interactions.\n"
+	text_augmented_prompt += "Pay attention to things like size, text, position, and color of all the elements, as well as the overall layout.\n"
+	text_augmented_prompt += "Respond with the content of the HTML+CSS file (directly start with the code, do not add any additional explanation):\n"
+
+	## call GPT-4V
+	html, prompt_tokens, completion_tokens, cost = gpt4v_call(openai_client, orig_input_image, text_augmented_prompt)
+
+	if auto_insertion:
+		## put texts back into marker positions 
+		for index, text in text_dict.items():
+			html = html.replace(f"[{index}]", text)
+	else:
+		# ## take screenshot of the generated marker webpage
+		# with open(image_file.replace(".png", "_marker.html"), "w") as f:
+		# 	f.write(html)
+		# take_screenshot(image_file.replace(".png", "_marker.html"), image_file.replace(".png", "_marker.png"))
+		# generated_marker_image = encode_image(image_file.replace(".png", "_marker.png"))
+
+		## the text insertion prompt
+		text_augmented_prompt = ""
+		text_augmented_prompt += "You are an expert web developer who specializes in HTML and CSS.\n"
+		text_augmented_prompt += "A user will provide you with a screenshot of a webpage. The implementation of this webpage with markers is:\n\n"
+		text_augmented_prompt += html + "\n"
+		text_augmented_prompt += "The original text elements are:\n" + texts + "\n"
+		text_augmented_prompt += "Your task is to insert the corresponding text elements back into the marker positions (replace all the markers with actual text content) so that the resultant webpage will look the same as the given one..\n"
+		text_augmented_prompt += "You need to return a single html file that uses HTML and CSS. Include all CSS code in the HTML file itself.\n"
+		text_augmented_prompt += "If it involves any images, use \"rick.jpg\" as the placeholder.\n"
+		text_augmented_prompt += "Directly edit the given HTML implementation. Do not change the layout structure of the webpage, just insert the text elements into appropriate positions.\n"
+		text_augmented_prompt += "Do not hallucinate any dependencies to external files. You do not need to include JavaScript scrips for dynamic interactions.\n"
+		text_augmented_prompt += "Pay attention to things like size, text, position, and color of all the elements, as well as the overall layout.\n"
+		text_augmented_prompt += "Respond with the content of the HTML+CSS file (directly start with the code, do not add any additional explanation):\n"
+
+		## call GPT-4V
+		html, prompt_tokens, completion_tokens, cost = gpt4v_call(openai_client, orig_input_image, text_augmented_prompt)
+
+	## remove the marker files
+	os.remove(image_file.replace(".png", "_marker.html"))
+	os.remove(image_file.replace(".png", "_marker.png"))
+
+	return html, prompt_tokens, completion_tokens, cost
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--prompt_method', type=str, default='text_augmented_prompting', help='prompting method to be chosen from {direct_prompting, text_augmented_prompting, revision_prompting}')
+	parser.add_argument('--prompt_method', type=str, default='text_augmented_prompting', help='prompting method to be chosen from {direct_prompting, text_augmented_prompting, revision_prompting, layout_marker_prompting}')
 	parser.add_argument('--orig_output_dir', type=str, default='gpt4v_text_augmented_prompting', help='directory of the original output that will be further revised')
 	parser.add_argument('--file_name', type=str, default='all', help='any particular file to be tested')
 	parser.add_argument('--subset', type=str, default='testset_100', help='evaluate on the full testset or just a subset (choose from: {testset_100, testset_full})')
 	parser.add_argument('--take_screenshot', action="store_true", help='whether to render and take screenshot of the webpages')
+	parser.add_argument('--auto_insertion', type=bool, default=False, help='whether to automatically insert texts into marker positions')
 	args = parser.parse_args()
 
 	## track usage
@@ -242,6 +316,8 @@ if __name__ == "__main__":
 		predictions_dir = cache_dir + "gpt4v_direct_prompting"
 	elif args.prompt_method == "text_augmented_prompting":
 		predictions_dir = cache_dir + "gpt4v_text_augmented_prompting"
+	elif args.prompt_method == "layout_marker_prompting":
+		predictions_dir = cache_dir + "gpt4v_layout_marker_prompting" + ("_auto_insertion" if args.auto_insertion else "") 
 	elif args.prompt_method == "revision_prompting":
 		predictions_dir = cache_dir + "gpt4v_visual_revision_prompting"
 		orig_data_dir = cache_dir + args.orig_output_dir
@@ -259,25 +335,28 @@ if __name__ == "__main__":
 	else:
 		test_files = [args.file_name]
 
-	for filename in tqdm(test_files[:1]):
-		try:
-			if args.prompt_method == "direct_prompting":
-				html, prompt_tokens, completion_tokens, cost = direct_prompting(openai_client, os.path.join(test_data_dir, filename))
-			elif args.prompt_method == "text_augmented_prompting":
-				html, prompt_tokens, completion_tokens, cost = text_augmented_prompting(openai_client, os.path.join(test_data_dir, filename))
-			elif args.prompt_method == "revision_prompting":
-				html, prompt_tokens, completion_tokens, cost = visual_revision_prompting(openai_client, os.path.join(test_data_dir, filename), os.path.join(orig_data_dir, filename))
+	for filename in tqdm(test_files):
+		if filename.endswith("2.png") or filename.endswith("5.png"):
+			try:
+				if args.prompt_method == "direct_prompting":
+					html, prompt_tokens, completion_tokens, cost = direct_prompting(openai_client, os.path.join(test_data_dir, filename))
+				elif args.prompt_method == "text_augmented_prompting":
+					html, prompt_tokens, completion_tokens, cost = text_augmented_prompting(openai_client, os.path.join(test_data_dir, filename))
+				elif args.prompt_method == "revision_prompting":
+					html, prompt_tokens, completion_tokens, cost = visual_revision_prompting(openai_client, os.path.join(test_data_dir, filename), os.path.join(orig_data_dir, filename))
+				elif args.prompt_method == "layout_marker_prompting":
+					html, prompt_tokens, completion_tokens, cost = layout_marker_prompting(openai_client, os.path.join(test_data_dir, filename), auto_insertion=args.auto_insertion)
 
-			total_prompt_tokens += prompt_tokens
-			total_completion_tokens += completion_tokens
-			total_cost += cost
+				total_prompt_tokens += prompt_tokens
+				total_completion_tokens += completion_tokens
+				total_cost += cost
 
-			with open(os.path.join(predictions_dir, filename.replace(".png", ".html")), "w") as f:
-				f.write(html)
-			if args.take_screenshot:
-				take_screenshot(os.path.join(predictions_dir, filename.replace(".png", ".html")), os.path.join(predictions_dir, filename))
-		except:
-			continue 
+				with open(os.path.join(predictions_dir, filename.replace(".png", ".html")), "w") as f:
+					f.write(html)
+				if args.take_screenshot:
+					take_screenshot(os.path.join(predictions_dir, filename.replace(".png", ".html")), os.path.join(predictions_dir, filename))
+			except:
+				continue 
 
 	## save usage
 	usage = {
--- Pix2Code/prompting/gpt4v.sh
+++ Pix2Code/prompting/gpt4v.sh
@@ -1,20 +1,34 @@
+# python3 gpt4v.py \
+#  --prompt_method direct_prompting \
+#  --file_name all \
+#  --subset testset_full \
+#  --take_screenshot
+
+
+# python3 gpt4v.py \
+#  --prompt_method text_augmented_prompting \
+#  --file_name all \
+#  --subset testset_full \
+#  --take_screenshot
+
+
+# python3 gpt4v.py \
+#  --prompt_method revision_prompting \
+#  --file_name all \
+#  --subset testset_full \
+#  --orig_output_dir "gpt4v_text_augmented_prompting" \
+#  --take_screenshot
+
 python3 gpt4v.py \
- --prompt_method direct_prompting \
- --file_name all \
- --subset testset_full \
+ --prompt_method layout_marker_prompting \
+ --file_name "1895.png" \
+ --subset testset_100 \
  --take_screenshot
 
 
-python3 gpt4v.py \
- --prompt_method text_augmented_prompting \
- --file_name all \
- --subset testset_full \
- --take_screenshot
-
-
-python3 gpt4v.py \
- --prompt_method revision_prompting \
- --file_name all \
- --subset testset_full \
- --orig_output_dir "gpt4v_text_augmented_prompting" \
- --take_screenshot
+#  python3 gpt4v.py \
+#  --prompt_method layout_marker_prompting \
+#  --file_name all \
+#  --subset testset_100 \
+#  --auto_insertion \
+#  --take_screenshot
